670327cc0e8a594f89b02ca2b9b33d6882fb145e
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2024-10-17 10:12:52 +0800

    Create build_opencv.sh

---
+++ build_opencv.sh
@@ -0,0 +1,55 @@
+install_dependencies () {
+    # open-cv has a lot of dependencies, but most can be found in the default
+    # package repository or should already be installed (eg. CUDA).
+    echo "Installing build dependencies."
+    sudo apt-get update
+    sudo apt-get dist-upgrade -y --autoremove
+    sudo apt-get install -y \
+        build-essential \
+        cmake \
+        git \
+        gfortran \
+        libatlas-base-dev \
+        libavcodec-dev \
+        libavformat-dev \
+        libavresample-dev \
+        libcanberra-gtk3-module \
+        libdc1394-22-dev \
+        libeigen3-dev \
+        libglew-dev \
+        libgstreamer-plugins-base1.0-dev \
+        libgstreamer-plugins-good1.0-dev \
+        libgstreamer1.0-dev \
+        libgtk-3-dev \
+        libjpeg-dev \
+        libjpeg8-dev \
+        libjpeg-turbo8-dev \
+        liblapack-dev \
+        liblapacke-dev \
+        libopenblas-dev \
+        libpng-dev \
+        libpostproc-dev \
+        libswscale-dev \
+        libtbb-dev \
+        libtbb2 \
+        libtesseract-dev \
+        libtiff-dev \
+        libv4l-dev \
+        libxine2-dev \
+        libxvidcore-dev \
+        libx264-dev \
+        pkg-config \
+        python-dev \
+        python-numpy \
+        python3-dev \
+        python3-numpy \
+        python3-matplotlib \
+        qv4l2 \
+        v4l-utils \
+        zlib1g-dev
+}
+
+sudo make install 
+sudo ldconfig 
+make clean 
+sudo apt update
